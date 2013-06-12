660d5a47d68fb64ad1bfaaef15c74d4b7ad8ae7a
Kenneth Kostenbader <svensken@gyges.rutgers.edu>
2013-06-12 18:11:29 -0400

    added old site

---
+++ old/People_files/People.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20120904
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('People_files/PeopleMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
